884d601801f86d794b19c416e88e22d42c0da9e6
Luke Gartner <lukegartner4@gmail.com>
2023-05-08 17:35:59 -0500

    generates input and table fields dynamically

--- index.js
+++ index.js
@@ -13,17 +13,19 @@ let isEditing = false;
 let editIndex = NaN;
 
 // DOM variables
-const firstNameInput = document.querySelector("#first-name");
-const lastNameInput = document.querySelector("#last-name");
-const idInput = document.querySelector("#id-number");
-const jobTitleInput = document.querySelector("#job-title");
-const annualSalaryInput = document.querySelector("#annual-salary");
+let firstNameInput = document.querySelector("#first-name");
+let lastNameInput = document.querySelector("#last-name");
+let idInput = document.querySelector("#id-number");
+let jobTitleInput = document.querySelector("#job-title");
+let annualSalaryInput = document.querySelector("#annual-salary");
 const monthlyBudgetInput = document.querySelector("#monthly-budget");
 const tableBody = document.querySelector("tbody");
 const totalMonthlySpan = document.querySelector(".total-monthly-span");
 const monthlyBudgetSpan = document.querySelector(".monthly-budget-span");
 const submitBtn = document.querySelector(".submit-btn");
-const sortSpans = document.querySelectorAll(".sort");
+let sortSpans = document.querySelectorAll(".sort");
+const tableHeaderRow = document.querySelector("#table-header-row");
+const inputContainer = document.querySelector(".input-container");
 
 // Handle Submit
 const handleSubmit = (e) => {
@@ -92,9 +94,6 @@ const formatSalary = (salary) => {
   }).format(salary);
 };
 
-// Display employees data when page initially loads
-displayData(employees);
-
 // remove employee
 const removeEmployee = (e) => {
   employees.splice(e.currentTarget.parentElement.dataset.id, 1);
@@ -125,20 +124,7 @@ const setLocalStorage = () => {
 };
 
 // Sort
-// // Add Event Listeners to spans
-for (let sortSpan of sortSpans) {
-  sortSpan.addEventListener("click", () => {
-    displayData(
-      sort(
-        employees,
-        sortSpan.dataset.property,
-        sortSpan.dataset.direction,
-        sortSpan.dataset.type
-      )
-    );
-    setLocalStorage();
-  });
-}
+
 // // Sort Function
 const sort = (unsorted, property, direction, type) => {
   if (type === "number") {
@@ -149,7 +135,7 @@ const sort = (unsorted, property, direction, type) => {
       return unsorted.sort((a, b) => b[property] - a[property]);
     }
   }
-  if (type === "string") {
+  if (type === "text") {
     if (direction === "ascending") {
       return unsorted.sort((a, b) =>
         a[property].toLowerCase() < b[property].toLowerCase() ? -1 : 1
@@ -170,4 +156,104 @@ const setMonthlyBudget = (e) => {
   monthlyBudget = Number(monthlyBudgetInput.value);
   setLocalStorage();
   displayData(employees);
+};
+
+// ---------------------------------------------------------------------------------
+// Create Fields for inputs and table
+window.addEventListener("DOMContentLoaded", () => {
+  generateTableHeaders(fieldsData);
+  generateInputs(fieldsData);
+  // Display employees data when page initially loads
+  displayData(employees);
+});
+
+// Fields Data
+const fieldsData = [
+  {
+    title: "First Name",
+    property: "firstName",
+    type: "text",
+    id: "first-name",
+  },
+  { title: "Last Name", property: "lastName", type: "text", id: "last-name" },
+  { title: "ID#", property: "id", type: "number", id: "id-number" },
+  { title: "Job Title", property: "jobTitle", type: "text", id: "job-title" },
+  {
+    title: "Annual Salary",
+    property: "annualSalary",
+    type: "number",
+    id: "annual-salary",
+  },
+];
+
+// Generate Inputs
+const generateInputs = (inputsData) => {
+  inputContainer.innerHTML = inputsData
+    .map(({ title, type, id }) => {
+      return `
+          <div class="input">
+            <label for="${id}">${title}</label>
+            <input
+              type="${type}"
+              name="${id}"
+              id="${id}"
+              placeholder="${title}"
+            />
+          </div>
+    `;
+    })
+    .join("");
+
+  // Assign to variables
+  firstNameInput = document.querySelector("#first-name");
+  lastNameInput = document.querySelector("#last-name");
+  idInput = document.querySelector("#id-number");
+  jobTitleInput = document.querySelector("#job-title");
+  annualSalaryInput = document.querySelector("#annual-salary");
+};
+
+// Generate Table Headers
+const generateTableHeaders = (headersData) => {
+  tableHeaderRow.innerHTML = headersData
+    .map(({ title, property, type }) => {
+      return `
+            <th>
+              ${title}
+              <span
+                class="sort"
+                data-property="${property}"
+                data-direction="ascending"
+                data-type="${type}"
+                ><i class="fa-solid fa-caret-up up"></i
+              ></span>
+              <span
+                class="sort"
+                data-property="${property}"
+                data-direction="descending"
+                data-type="${type}"
+                ><i class="fa-solid fa-caret-down down"></i
+              ></span>
+            </th>
+    `;
+    })
+    .concat(["<th></th><th></th>"])
+    .join("");
+
+  // Assign spans to sortSpans Variable
+  sortSpans = document.querySelectorAll(".sort");
+
+  // // Add Event Listeners to spans
+  for (let sortSpan of sortSpans) {
+    sortSpan.addEventListener("click", () => {
+      displayData(
+        sort(
+          employees,
+          sortSpan.dataset.property,
+          sortSpan.dataset.direction,
+          sortSpan.dataset.type
+        )
+      );
+      setLocalStorage();
+    });
+  }
 };
